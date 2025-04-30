0e5e44eed65635e5704eee31ff0b3b9a4c375dbf
Mohammed Nasim <75156273+nasimstg@users.noreply.github.com>
2025-05-01 01:42:43 +0600

    init- technewstoday

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,10 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
 import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 import SearchButton from './SearchButton'
+import Image from 'next/image'
 
 const Header = () => {
   let headerClass = 'flex items-center w-full bg-white dark:bg-gray-950 justify-between py-10'
@@ -17,7 +17,13 @@ const Header = () => {
       <Link href="/" aria-label={siteMetadata.headerTitle}>
         <div className="flex items-center justify-between">
           <div className="mr-3">
-            <Logo />
+            <Image
+              src={siteMetadata.headerLogo}
+              alt="technewstoday.site logo"
+              width={40}
+              height={40}
+              className="h-10 w-10 rounded-full"
+            />
           </div>
           {typeof siteMetadata.headerTitle === 'string' ? (
             <div className="hidden h-6 text-2xl font-semibold sm:block">
